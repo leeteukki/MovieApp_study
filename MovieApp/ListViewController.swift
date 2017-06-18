8ce385144bc412f47eb172de5a5d7092d15c8e28
Lena Lee <lenalee@Lenaui-MacBook-Pro.local>
2017-06-18 16:52:35 +0900

    use dataset

--- MovieApp/ListViewController.swift
+++ MovieApp/ListViewController.swift
@@ -11,38 +11,54 @@ import UIKit
 class ListViewController: UITableViewController {
     
     
-    var list = [MovieVO]()
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해서 예술이 되다", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯걸음", "2015-05-07", 9.19)
+    ]
+    
+    lazy var list : [MovieVO] = {
+        
+        var datalist = [MovieVO]()
+        
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            
+            mvo.title       = title
+            mvo.description = desc
+            mvo.openrate    = opendate
+            mvo.rating      = rating
+            
+            datalist.append(mvo)
+        }
+        
+        return datalist
+    }()
     
     override func viewDidLoad() {
-        var mvo = MovieVO()
-        
-        mvo.title       = "다크나이트"
-        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이되다"
-        mvo.openrate    = "2008-09-04"
-        mvo.rating      = 8.95
-        
-        
-        self.list.append(mvo)
-        
-        mvo = MovieVO()
-        
-        mvo.title       = "호우시절"
-        mvo.description = "때를 알고 내리는 좋은 비"
-        mvo.openrate    = "2009-10-08"
-        mvo.rating      = 7.31
-        
-        self.list.append(mvo)
-        
-        mvo.title       = "말할 수 없는 비밀"
-        mvo.description = "여기서 너까지 다섯 걸음"
-        mvo.openrate    = "2015-05-07"
-        mvo.rating      = 9.19
-        
-        self.list.append(mvo)
-        
         
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.list.count
+        
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let row = self.list[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        
+        return cell
+        
+        }
     
-}
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
+    
+        }
+    
+    }

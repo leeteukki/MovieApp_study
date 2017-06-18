b45b50fb7963f8a68ab7b5be7053bf94ddec9938
Lena Lee <lenalee@Lenaui-MacBook-Pro.local>
2017-06-18 19:08:36 +0900

    image

--- MovieApp/ListViewController.swift
+++ MovieApp/ListViewController.swift
@@ -12,22 +12,23 @@ class ListViewController: UITableViewController {
     
     
     var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해서 예술이 되다", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯걸음", "2015-05-07", 9.19)
+        ("다크나이트", "영웅물에 철학에 음악까지 더해서 예술이 되다", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯걸음", "2015-05-07", 9.19, "secret.jpg")
     ]
     
     lazy var list : [MovieVO] = {
         
         var datalist = [MovieVO]()
         
-        for (title, desc, opendate, rating) in self.dataset {
+        for (title, desc, opendate, rating, thumbnail) in self.dataset {
             let mvo = MovieVO()
             
             mvo.title       = title
             mvo.description = desc
             mvo.openrate    = opendate
             mvo.rating      = rating
+            mvo.thumbnail   = thumbnail
             
             datalist.append(mvo)
         }
@@ -51,18 +52,13 @@ class ListViewController: UITableViewController {
         let row = self.list[indexPath.row]
         
         //테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
-        let title    = cell.viewWithTag(101) as? UILabel
-        let desc     = cell.viewWithTag(102) as? UILabel
-        let opendate = cell.viewWithTag(103) as? UILabel
-        let rating   = cell.viewWithTag(104) as? UILabel
-        
-        title?.text    = row.title
-        desc?.text     = row.description
-        opendate?.text = row.openrate
-        rating?.text   = "\(row.rating!)"
-        
+        cell.title?.text     = row.title
+        cell.desc?.text      = row.description
+        cell.opendate?.text  = row.openrate
+        cell.rating?.text    = "\(row.rating!)"
+        cell.thumbnail.image = UIImage(named: row.thumbnail!)
         
         return cell
         
---
+++ MovieApp/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  MovieApp
+//
+//  Created by Lena Lee on 2017. 6. 18..
+//  Copyright © 2017년 Lena Lee. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+ 
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var desc: UILabel!
+    
+    @IBOutlet weak var rating: UILabel!
+    
+    @IBOutlet weak var opendate: UILabel!
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+}
